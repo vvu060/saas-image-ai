8760ccbced6e172aa912080a33d0c7e480d2fff4
vvu060 <vvu060@gmail.com>
2024-03-14 15:59:02 +0530

    create & use header comp dynamically

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '@/components/shared/Header';
+import { transformationTypes } from '@/constants';
+import { type } from 'os';
 
-const AddTransformationPage = () => {
-  return <div>AddTransformationPage</div>;
+const AddTransformationPage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <Header title={transformation.title} subtitle={transformation.subTitle} />
+  );
 };
 
 export default AddTransformationPage;
