a1018bced199201db54995d08609e78bf90edbc5
vvu060 <vvu060@gmail.com>
2024-03-15 11:21:16 +0530

    use form in all places

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import Header from '@/components/shared/Header';
 import { transformationTypes } from '@/constants';
-import { type } from 'os';
+import Header from '@/components/shared/Header';
+import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationPage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationPage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
   const transformation = transformationTypes[type];
+
+  if (!userId) redirect('/sign-in');
+
+  const user = await getUserById(userId);
   return (
-    <Header title={transformation.title} subtitle={transformation.subTitle} />
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <section className='mt-10'>
+        <TransformationForm
+          action='Add'
+          data={null}
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   );
 };
 
